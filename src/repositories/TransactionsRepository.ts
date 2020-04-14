afadcde280f0eb0aec3a87caafa1f3269dbd86f2
Rafael <rlisboars@gmail.com>
2020-04-13 22:56:51 -0300

    Complete version

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -13,16 +20,28 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): { transactions: Transaction[]; balance: Balance } {
+    return { transactions: this.transactions, balance: this.getBalance() };
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acc, curr) => {
+        const balanceAux = acc || { income: 0, outcome: 0, total: 0 };
+        if (curr.type === 'income') balanceAux.income += curr.value;
+        else balanceAux.outcome += curr.value;
+        balanceAux.total = balanceAux.income - balanceAux.outcome;
+        return balanceAux;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
